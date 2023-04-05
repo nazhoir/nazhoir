5ae0b37dcd40d6b4a47974e7f86ca5ab8636f762
nazhoir <nazhoirulamin031201@gmail.com>
2023-04-06 01:47:41 +0700

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,43 @@ import SiteNav from "@/components/site-hav";
 import "./globals.css";
 import SiteFooter from "@/components/site-footer";
 
+const title =
+	"Nazhoir | Writer, Proggrammer and Founder and CEO at Diskresi";
+const description =
+	"Driven programmer and writers taking on every challenge with gusto. Firmly believes that success is not just about the end result, but the journey itself. Ready for anything.";
+
 export const metadata = {
-	title:
-		"Nazhoir | Writer, Proggrammer and Founder and CEO at Diskresi",
-	description:
-		"Driven programmer and writers taking on every challenge with gusto. Firmly believes that success is not just about the end result, but the journey itself. Ready for anything.",
+	title,
+	description,
+	openGraph: {
+		title,
+		description,
+		type: "profile",
+	},
+	twitter: {
+		card: "summary",
+		title,
+		description,
+		siteId: "1072023881769136128",
+		creator: "@nazhoir",
+		creatorId: "1072023881769136128",
+		images: [
+			"https://avatars.githubusercontent.com/u/74437335?v=4?s=400",
+		],
+	},
+	robots: {
+		index: false,
+		follow: true,
+		nocache: true,
+		googleBot: {
+			index: true,
+			follow: false,
+			noimageindex: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
 };
 
 export default function RootLayout({
